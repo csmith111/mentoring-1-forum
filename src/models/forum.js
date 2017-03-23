0f90d504c08fca1a1ec4eff885def1711d4b15de
C Smith <codesmith111@gmail.com>
2017-03-23 12:58:37 -0400

    Updated forumSchema

--- src/models/forum.js
+++ src/models/forum.js
@@ -3,16 +3,17 @@ const mongoose=require('mongoose')
 mongoose.Promise = global.Promise
 const Schema=mongoose.Schema
 
-const forumSchema = new Schema({
-    topic:{type:String, required:true},
-    text:{type:String, required:true},
-    createdAt:{type:Date, required:true},
-    comments:[
-        {comment:{
-            text:{type:String, required:true}, 
-            createdAt:{type:Date, required:true}
-        }}
-    ]
-})
+const forumTopicsSchema = new Schema({
+    topics:[{
+        topic:{type:String, required:true},
+        text:{type:String, required:true},
+        createdAt:{type:Date, required:true},
+        comments:[
+            {comment:{
+                text:{type:String, required:true}, 
+                createdAt:{type:Date, required:true}
+            }}
+        ]}
+    ]})
 
-module.exports = mongoose.model('ToDoItem', forumSchema)
+module.exports = mongoose.model('forumTopics', forumTopicsSchema)
