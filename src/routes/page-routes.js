ffa44491fcd1afbe5cde14d51d160af8cd86cc46
C Smith <codesmith111@gmail.com>
2017-03-24 14:48:27 -0400

    completed all issues

--- src/routes/page-routes.js
+++ src/routes/page-routes.js
@@ -3,42 +3,16 @@ const express = require('express')
 
 const router = express.Router()
 
-
-const dataOld = [
-    {
-        topic: 'JavaScript is Tricky',
-        text: 'Have you had the feeling of wtf...wtf when using JavaScript',
-        createdAt: '1/1/2012',
-        user: { _id: 1, name: 'Joe' },
-    },
-    {
-        topic: 'JavaScript is Cool',
-        text: 'JavaScript has some bad parts but the good parts are great!',
-        createdAt: '1/1/2012',
-        user: { _id: 2, name: 'Jim' },
-    },
-]
-
-async function index(req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello' })
-}
-
 async function topics(req, res) {
     const data = await model.Topic.find()
     res.render('topics', { title: 'Topics', message: 'This will display a set of topics', topics: data })
-}
-
-async function topic(req, res) {
-    res.render('topic', { title: 'Topics', message: 'This is the form to add new topics' })
 }
 
 async function addTopic(req, res) {
     res.render('addTopic', { title: 'Add Topics', message: 'This is the form to add new topics' })
 }
 
-router.get('/', index)
 router.get('/topics', topics)
-router.get('/topic', topic)
 router.get('/addTopic', addTopic)
 
 module.exports = router
